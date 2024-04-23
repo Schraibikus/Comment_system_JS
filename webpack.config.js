e6a60575ae5365504324cb1e93ca2bfad2103bd9
Roman Ulitin <kurt-77@mail.ru>
2024-04-23 20:04:50 +0500

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: path.resolve(__dirname, "src/js/index.js"),
   output: {
@@ -7,13 +10,26 @@ module.exports = {
     filename: "main.js",
   },
   mode: "production",
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.pug",
+      filename: "index.html",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+      {
+        test: /\.pug$/,
+        use: "pug-loader",
+      },
     ],
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
   },
 };
