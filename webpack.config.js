2a8d441ec263a643d94e48aecf905b45e2d804ca
Roman Ulitin <kurt-77@mail.ru>
2024-04-23 14:42:35 +0500

    added mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
+var path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: path.resolve(__dirname, "src/js/index.js"),
   output: {
+    path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
   mode: "production",
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
